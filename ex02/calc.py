bf2f0b7ec07f11fd5d52ec91de44ce1b2964197f
C0A21093 <c0a2109372@edu.teu.ac.jp>
2022-11-29 15:21:51 +0900

    c_tuika

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,12 +20,26 @@ entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row = 0, column=0,columnspan=3)
 
 r, c = 1, 0
+
+command_list = ["c"]
+for cm in command_list:
+    button = tk.Button(root, text=cm, width=4, height=2, font=("",30))
+    button.grid(row=r, column=c)
+    c+=1
+    if c%3 == 0:
+        r += 1
+        c = 0
+    
+
+
+r += 1
+c = 0
 for i in range(10):
     button = tk.Button(root, text=f"{9-i}", width=4, height=2, font=("",30))
     button.grid(row = r, column=c)
     button.bind("<1>",button_click)
     c+=1
-    if i%3==0 and i != 0:
+    if i%3==2 and i != 0:
         r+=1
         c=0
         
