7d234649d31538fb9ecbc8f66ec659e1c26be48a
C0A21093 <c0a2109372@edu.teu.ac.jp>
2022-11-29 14:23:50 +0900

    toi2

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 
 root = tk.Tk()
-
-
+r, c = 0, 0
+for i in range(9, -1, -1):
+    button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
+    button.grid(row = r, column=c)
+    #button.bind("<1>",button_click)
+    c+=1
+    if i%3==1:
+        r+=1
+        c=0
 root.mainloop()
