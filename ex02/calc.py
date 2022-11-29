18b4c475c0aa4f96108eeabb00fde88793a7d8ff
C0A21093 <c0a2109372@edu.teu.ac.jp>
2022-11-29 14:59:23 +0900

    original

--- ex02/calc.py
+++ ex02/calc.py
@@ -20,12 +20,12 @@ entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row = 0, column=0,columnspan=3)
 
 r, c = 1, 0
-for i in range(9, -1, -1):
-    button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
+for i in range(10):
+    button = tk.Button(root, text=f"{9-i}", width=4, height=2, font=("",30))
     button.grid(row = r, column=c)
     button.bind("<1>",button_click)
     c+=1
-    if i%3==1:
+    if i%3==0 and i != 0:
         r+=1
         c=0
         
