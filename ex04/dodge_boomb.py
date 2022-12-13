05a058e7512d1e62f84e073ec618d873c6c8a00d
C0A21093 <c0a2109372@edu.teu.ac.jp>
2022-12-13 14:46:13 +0900

    rensyuu5

--- ex04/dodge_boomb.py
+++ ex04/dodge_boomb.py
@@ -1,4 +1,4 @@
-import pygame as pg, sys
+import pygame as pg, sys, random
 
 def main():
     #以降ゲーム処理
@@ -6,6 +6,7 @@ def main():
     #練習１
     pg.display.set_caption("逃げろ！こうかとん") #タイトルバー表示
     scrn_sfc = pg.display.set_mode((1600, 900)) #Surfaceクラスのオブジェクトを生成
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg") #Surface
     pgbg_rct = pgbg_sfc.get_rect()
     
@@ -14,6 +15,16 @@ def main():
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect() #Rect
     tori_rct.center = 900, 400
+    
+    #練習5
+    boomb_sfc = pg.Surface((20, 20))#正方形空Surface
+    boomb_sfc.set_colorkey(0, 0)#黒い部分を透明化
+    pg.draw.circle(boomb_sfc, (255, 0, 0), (10, 10), 10)
+    boomb_rct = boomb_sfc.get_rect()
+    boomb_rct.centerx = random.randint(0, scrn_rct.width)
+    boomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(boomb_sfc, boomb_rct) #blit
+    
     
     #練習２
     while True:
@@ -37,6 +48,7 @@ def main():
             tori_rct.centerx += 1
         scrn_sfc.blit(tori_sfc, tori_rct) #blit
         
+        scrn_sfc.blit(boomb_sfc, boomb_rct)
         pg.display.update()
         clock.tick(1000) #1000fps
             
