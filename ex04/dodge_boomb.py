b0315dd6425168039d14e048b302984b2d136c2d
C0A21093 <c0a2109372@edu.teu.ac.jp>
2022-12-13 17:40:38 +0900

    追加

--- ex04/dodge_boomb.py
+++ ex04/dodge_boomb.py
@@ -68,9 +68,14 @@ def main():
             bomb[3] *= check_bound(bomb[1], scrn_rct)[1]
                     
         if count%1000 == 1: #１秒ごとにボールを追加する
-            boomb_sfc = pg.Surface((20, 20))#正方形空Surface
-            boomb_sfc.set_colorkey(0, 0)#黒い部分を透明化
-            pg.draw.circle(boomb_sfc, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (10, 10), 10) #色をランダムに決定する
+            if random.randint(1, 4) == 2: #一定確率で大きい爆弾
+                boomb_sfc = pg.Surface((40, 40))#正方形空Surface
+                boomb_sfc.set_colorkey(0, 0)#黒い部分を透明化
+                pg.draw.circle(boomb_sfc, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (20, 20), 30) #色をランダムに決定する
+            else:
+                boomb_sfc = pg.Surface((20, 20))#正方形空Surface
+                boomb_sfc.set_colorkey(0, 0)#黒い部分を透明化
+                pg.draw.circle(boomb_sfc, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (10, 10), 10) #色をランダムに決定する
             boomb_rct = boomb_sfc.get_rect()
             boomb_rct.centerx = random.randint(1, scrn_rct.width) #範囲を1からにすることでバグ修正
             boomb_rct.centery = random.randint(1, scrn_rct.height) #範囲を1からにすることでバグ修正
